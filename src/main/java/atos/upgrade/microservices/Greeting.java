e2e616fb5750d7fc2f0e7bd7e0a1c35c32f49ce8
Hazzim-Escarcega <said-hazzim.escarcega@atos.net>
2022-05-16 11:50:47 -0600

    Video 2 Greeting api

---
+++ src/main/java/atos/upgrade/microservices/Greeting.java
@@ -0,0 +1,27 @@
+package atos.upgrade.microservices;
+
+public class Greeting {
+    private Long id;
+    private String message;
+
+    public Greeting(Long id, String message) {
+        this.id = id;
+        this.message = message;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
